cb5bb9935f4b692cbf8ac6fa73dfea4c65847dae
Arpit Satyal <arpited7@gmail.com>
2019-10-17 22:16:17 +0545

    firebase and firestore basics

---
+++ app.js
@@ -0,0 +1,61 @@
+const ul = document.querySelector('ul')
+const form = document.querySelector('form')
+const button = document.querySelector('button')
+
+function addRecipe(recipe, id) {
+    let time = recipe.created_at.toMillis()
+    let now = new Date().getTime()
+    let formatted = dateFns.distanceInWords(now, time, { addSuffix: true })
+    let html = `<li data-id="${id}">
+    <div> ${recipe.title}</div>
+    <div> ${formatted} </div>
+    <button class="btn btn-danger btn-sm my-2"> delete </button> </li>`
+    ul.innerHTML += html
+}
+
+function deteleRecipe(id) {
+    const recipes = document.querySelectorAll('li')
+    recipes.forEach(recipe => {
+        if (recipe.getAttribute('data-id') === id) {
+            recipe.remove()
+        }
+    })
+}
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    let now = new Date()
+    let recipe = {
+        title: form.recipe.value,
+        created_at: firebase.firestore.Timestamp.fromDate(now)
+    }
+    db.collection('recipes').add(recipe)
+        .then(() => console.log('recipe added'))
+        form.reset()
+})
+
+const unSub = db.collection('recipes').onSnapshot(snapshot => {
+    console.log(snapshot.docChanges())
+    snapshot.docChanges().forEach(change => {
+        const doc = change.doc
+        if (change.type === 'added') {
+            addRecipe(doc.data(), doc.id)
+        } else if (change.type === 'removed') {
+            deteleRecipe(doc.id)
+        }
+    })
+})
+
+ul.addEventListener('click', e => {
+    if (e.target.tagName === 'BUTTON') {
+        let id = e.target.parentElement.getAttribute('data-id')
+        db.collection('recipes').doc(id).delete()
+            .then(() => console.log('deleted'))
+    }
+})
+
+//unsubscribe from changes
+button.addEventListener('button', () => {
+    unSub()
+    console.log('un subscribed from collection changes.')
+})
---
+++ firebase.js
@@ -0,0 +1,12 @@
+ // Your web app's Firebase configuration
+ var firebaseConfig = {
+    apiKey: "AIzaSyC6Ia9F-NLRW-kvzmUk4Z0jPALnGy6dDXE",
+    authDomain: "arpit-js.firebaseapp.com",
+    databaseURL: "https://arpit-js.firebaseio.com",
+    projectId: "arpit-js",
+    storageBucket: "arpit-js.appspot.com",
+    messagingSenderId: "95923665214",
+}
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore(); 
